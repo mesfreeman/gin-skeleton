a594b56a5c52cef855987cbab80874d1e8c55f6f
贺全喜 <hequanxi@bianfeng.com>
2021-06-29 15:01:30 +0800

    优化gorm时间处理逻辑

---
+++ model/base.go
@@ -0,0 +1,37 @@
+package model
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"time"
+)
+
+// 自定义Time格式
+type FormatTime struct {
+	Time time.Time
+}
+
+// 重写 MarshaJSON 方法，实现格式化时间
+func (f FormatTime) MarshalJSON() ([]byte, error) {
+	output := fmt.Sprintf("\"%s\"", f.Time.Format("2006-01-02 15:04:05"))
+	return []byte(output), nil
+}
+
+// 重写 Value 方法，自定义时间格式写入数据库
+func (f FormatTime) Value() (driver.Value, error) {
+	var zeroTime time.Time
+	if f.Time.UnixNano() == zeroTime.UnixNano() {
+		return nil, nil
+	}
+	return f.Time, nil
+}
+
+// 重写 Scan 方法，读取时自动转换
+func (f *FormatTime) Scan(v interface{}) error {
+	value, ok := v.(time.Time)
+	if ok {
+		*f = FormatTime{Time: value}
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to timestamp", v)
+}
--- model/test.go
+++ model/test.go
@@ -1,15 +1,16 @@
 package model
 
-import (
-	"time"
-)
-
 type TestModel struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	Age       int       `json:"age"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        int        `json:"id"`
+	Name      string     `json:"name"`
+	Age       int        `json:"age"`
+	CreatedAt FormatTime `json:"created_at"`
+	UpdatedAt FormatTime `json:"updated_at"`
+}
+
+// 表名
+func (TestModel) TableName() string {
+	return "test"
 }
 
 // 创建
